b80f4a7603c07524b955d6602c6f0785b0209e00
wallstedt <fredrik.wallstedt@gmail.com>
2017-08-01 09:52:31 +0200

    find greatest number in list

--- random-numbers.py
+++ random-numbers.py
@@ -1,6 +1,17 @@
 import random
 
-magic_numbers = [random.randint(0, 9), random.randint(0,9)]
+magic_numbers = []
+greatest = -1
+# generate 10 random numbers
+for number in range(0, 9):
+    num = random.randint(0, 9)
+    magic_numbers.insert(num, number)
+    if num > greatest:
+        greatest = num
+# magic_numbers = [random.randint(0, 9), random.randint(0,9)]
+
+print("The greatest number is {}".format(greatest))
+
 chances = 3
 
 for attempt in range(chances): # range(chances) is [0,1,2]
